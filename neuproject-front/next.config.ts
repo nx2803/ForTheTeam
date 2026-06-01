7af06c2e65961349e2ee1918bdfdab5f015c25c4
nx2803 <nx2803@gmail.com>
2026-06-02 02:22:03 +0900

    feat: configure next.config.ts with image remote patterns and component caching

--- neuproject-front/next.config.ts
+++ neuproject-front/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
-  experimental: {
-    ppr: 'incremental',
-  },
   images: {
     remotePatterns: [
       {
